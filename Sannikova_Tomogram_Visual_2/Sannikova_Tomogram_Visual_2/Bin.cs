9a60bf84b781d9e0de4ad4a82a0d3ef70901b9bd
VikaSannikova <vikasannikova1998@gmail.com>
2017-04-12 14:05:06 +0300

    начало положено

---
+++ Sannikova_Tomogram_Visual_2/Sannikova_Tomogram_Visual_2/Bin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sannikova_Tomogram_Visual_2
+{
+    class Bin
+    {
+    }
+}
